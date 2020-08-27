3f60ae56e45f9a067ed8164027d5da4d293d276d
Alejandro López Ramírez <alejozepol@gmail.com>
2020-08-26 22:09:34 -0500

    firebase auth email

--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -16,27 +16,128 @@ class Firebase {
     }
   }
 
+  // Metodos Base de datos
+
+  async getDataColletion(collection) {
+    const data = await new Promise((resolve, reject) => {
+      FirebaseApp.firestore().collection(collection)
+        .onSnapshot(({ docs }) => {
+          const d = [];
+          docs.forEach((doc) => {
+            const details = {
+              id: doc.id,
+              ...doc.data(),
+            };
+            d.push(details);
+            resolve(d);
+          });
+        });
+    });
+    return data;
+  };
+
+  async query1(collection, query) {
+    const data = await new Promise((resolve, reject) => {
+      FirebaseApp.firestore().collection(collection)
+        .where(query.name, '==', query.data)
+        .onSnapshot(({ docs }) => {
+          const d = [];
+          docs.forEach((doc) => {
+            const details = {
+              id: doc.id,
+              ...doc.data(),
+            };
+            d.push(details);
+            resolve(d);
+          });
+        });
+    });
+    return data;
+  };
+
+  async query2(collection, query1, query2) {
+    const data = await new Promise((resolve) => {
+      FirebaseApp.firestore().collection(collection)
+        .where(query1.name, '==', query1.data)
+        .where(query2.name, '==', query2.data)
+        .onSnapshot(({ docs }) => {
+          const d = [];
+          docs.forEach((doc) => {
+            const details = {
+              id: doc.id,
+              ...doc.data(),
+            };
+            d.push(details);
+            resolve(d);
+          });
+        });
+    });
+    return data;
+  };
+
+  async createSet(collection, data) {
+    const createSet = await new Promise(() => {
+      FirebaseApp.firestore().collection(collection)
+        .doc(String(data.id))
+        .set(data)
+        .then()
+        .catch((error) => console.log(error));
+    })
+      .catch((e) => console.error(e));
+    return createSet;
+  };
+
+  async create(collection, data) {
+    const create = await new Promise(() => {
+      FirebaseApp.firestore().collection(collection)
+        .add(data)
+        .catch((error) => error);
+    });
+    return create;
+  };
+
+  async delete(collection, id) {
+    await new Promise(() => {
+      FirebaseApp.firestore().collection(collection)
+        .doc(id)
+        .delete()
+        .catch((error) => error);
+    });
+  };
+
+  async getDocument(collection, id) {
+    const getDocument = await new Promise((resolve) => {
+      FirebaseApp.firestore().collection(collection)
+        .doc(id)
+        .get()
+        .then((doc) => resolve(doc.data()))
+        .catch((error) => error);
+    });
+    return getDocument;
+  };
+
+//auth
+
   async createUserWithEmailAndPassword(user, url) {
     const data = {};
 
     return await new Promise((resolve, reject) => {
-      this.auth
+      FirebaseApp.auth()
         .createUserWithEmailAndPassword(user.EMAIL, user.PASSWORD)
         .then((res) => {
           delete user.PASSWORD;
           data.id = user.EMAIL;
           data.uid = res.user.uid;
           data.EMAIL = user.EMAIL;
-          data.photoURL = user.photoURL;
           data.name = user.name;
           res.user.sendEmailVerification({
             url,
           });
           res.user.updateProfile({
             displayName: user.name,
-            photoURL: user.photoURL,
           });
-
+          this.createSet('user', data);
+          window.location.href = '/login';
         })
         .catch((error) => {
           switch (error.code) {
@@ -52,16 +153,11 @@ class Firebase {
 
   async loginMail(user) {
     return await new Promise((resolve, reject) => {
-      console.log(user.EMAIL);
-      this.auth.signInWithEmailAndPassword(user.EMAIL, user.PASSWORD)
+      FirebaseApp.auth().signInWithEmailAndPassword(user.EMAIL, user.PASSWORD)
         .then(() => {
           this.getDocument('user', user.EMAIL)
             .then((data) => {
-              document.cookie = `EMAIL=${data.EMAIL}`;
-              document.cookie = `name=${data.name}`;
-              document.cookie = `id=${data.uid}`;
-              document.cookie = `photoURL=${data.photoURL}`;
-              window.location.href = '/';
+              document.cookie = `bidder=${JSON.stringify(data)}; max-age=3600; path=/`;
             });
         })
         .catch((error) => {
@@ -78,50 +174,49 @@ class Firebase {
 
   async loginGoogle() {
     const data = {};
-   return FirebaseApp.auth()
-      .signInWithPopup(new FirebaseApp.auth.GoogleAuthProvider())
-      .then((res) => {
-        data.photoURL = res.user.photoURL;
-        data.name = res.user.displayName;
-        data.uid = res.user.uid;
-        data.id = res.user.email;
-        data.EMAIL = res.user.email;
-        this.createSet('user', data);
-        document.cookie = `EMAIL=${data.EMAIL}`;
-        document.cookie = `name=${data.name}`;
-        document.cookie = `id=${data.uid}`;
-        document.cookie = `photoURL=${data.photoURL}`;
-        window.location.href = '/';
-      })
-      .catch((error) => {
-        console.log(error);
-      })
+    return new Promise((resolve, reject) => {
+      FirebaseApp.auth()
+        .signInWithPopup(new FirebaseApp.auth.GoogleAuthProvider())
+        .then((res) => {
+          console.log(res)
+          data.photoURL = res.user.photoURL;
+          data.name = res.user.displayName;
+          data.uid = res.user.uid;
+          data.id = res.user.email;
+          data.EMAIL = res.user.email;
+          this.createSet('user', data);
+          document.cookie = `bidder=${JSON.stringify(data)}; max-age=3600; path=/`;
+          resolve(data)
+        })
+        .catch((error) => {
+          reject(new Error(error));
+        })
+    })
   }
-
   async loginFacebook() {
     const data = {};
-    return this.auth
-      .signInWithPopup(new FirebaseApp.auth.FacebookAuthProvider())
-      .then((res) => {
-        data.photoURL = res.user.photoURL;
-        data.name = res.user.displayName;
-        data.uid = res.user.uid;
-        data.id = res.user.email;
-        data.EMAIL = res.user.email;
-        this.createSet('user', data);
-        document.cookie = `EMAIL=${data.EMAIL}`;
-        document.cookie = `name=${data.name}`;
-        document.cookie = `id=${data.uid}`;
-        document.cookie = `photoURL=${data.photoURL}`;
-        window.location.href = '/';
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    return new Promise((resolve, reject) => {
+      FirebaseApp.auth()
+        .signInWithPopup(new FirebaseApp.auth.FacebookAuthProvider())
+        .then((res) => {
+          data.photoURL = res.user.photoURL;
+          data.name = res.user.displayName;
+          data.uid = res.user.uid;
+          data.id = res.user.email;
+          data.EMAIL = res.user.email;
+          this.createSet('user', data);
+          document.cookie = `bidder=${JSON.stringify(data)}; max-age=3600; path=/`;
+          resolve(data)
+        })
+        .catch((error) => {
+          reject(new Error(error));
+        })
+    })
   }
 
+
   async signOut() {
-    this.auth.signOut().catch((e) => console.error(e));
+    FirebaseApp.auth().signOut().catch((e) => console.error(e));
   }
 
 
--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,50 +1,92 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Firebase } from '../Firebase';
 import '../assets/styles/container/login.css'
 import Face from '../assets/imagenes/facebook.png'
 import Logo from '../componets/Logo'
+import { Link } from 'react-router-dom';
 
-const Login = () => {
+const Login = (props) => {
+    const [form, setValues] = useState({
+        EMAIL: '',
+    });
 
-    return(
+    const handleInput = (event) => {
+        setValues({
+            ...form,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handlSubmit = (event) => {
+        event.preventDefault(); //quitar el defecto del comportamiento de un formulario
+        Firebase.prototype.loginMail(form)
+            .then(() => {
+                console.log('formulario registrado con exito.')
+                props.history.push('/home');// rediriges a la pagina que quiero que se muestre si el formulario fue efectivo.
+            })
+            .catch(() => {
+                console.log('algo salio mal')
+            })
+        // Ejecutar el registro de auth para usuerio
+    };
+
+    const LoginGoogle = () => {
+        Firebase.prototype.loginGoogle()
+            .then((res) => {
+                props.history.push('/languaje')
+            })
+            .catch((e) => console.log(e))
+    }
+    const LoginFacebook = () => {
+        Firebase.prototype.loginFacebook()
+            .then((res) => {
+                props.history.push('/languaje')
+            })
+            .catch((e) => console.log(e))
+    }
+    return (
         <section className="mainLogin">
-            <Logo/>
-            <div className="div-inputs-login">
-                <div className="div-span-login">   
-                    <span className="labelsapan">E-mail:</span>
+            <Logo />
+            <form onSubmit={handlSubmit}>
+                <div className="div-inputs-login">
+                    <div className="div-span-login">
+                        <span className="labelsapan">E-mail:</span>
+                    </div>
+                    <input type="text" name="EMAIL" id="" onChange={handleInput} />
                 </div>
-                <input type="text" name="" id="" />
-            </div>
 
-            <div class="div-inputs-login">
-                <div className="div-span-login">   
-                    <span className="labelsapan">Password:</span>
+                <div class="div-inputs-login">
+                    <div className="div-span-login">
+                        <span className="labelsapan">Password:</span>
+                    </div>
+                    <input minLength='6' type="password" name="PASSWORD" id="" onChange={handleInput} />
                 </div>
-                <input type="text" name="" id="" />
-            </div>
-
-            <div className="div-face-login" >
-                <img className="img-face" src={Face} alt=""/>
-            </div>
-
-            <div className="div-button-login">
-                <button className="button-login">
-                    Login
+                <div className="div-button-login">
+                    <button type='submit' className="button-login">
+                        Login
                 </button>
-            </div>
+                </div>
+            </form>
+
+            <button className="div-face-login" onClick={() => LoginFacebook()}>
+                <img className="img-face" src={Face} alt="" />
+            </button>
+            <button type='button' onClick={() => LoginGoogle()}>Google</button>
+
+
             <div>
                 <div className="div-recover">
-                    <a href="recover.html">
+                    <Link to="/recover">
                         Did you forget your password
-                    </a>
+                    </Link>
                 </div>
-            
+
                 <div className="sign">
-                    <a className="sign-login" href="signup.html">
-                        Sign up</a>
+                    <Link className="sign-login" to="/sign">
+                        Sign up</Link>
                 </div>
             </div>
-    </section>
+        </section>
 
     )
 }
