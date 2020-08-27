f9a408edf8a3f69d08da2d80cd2e285857a288e2
Alejandro López Ramírez <alejozepol@gmail.com>
2020-08-26 19:05:28 -0500

    auth google

--- src/containers/SignUp.jsx
+++ src/containers/SignUp.jsx
@@ -10,66 +10,80 @@ const SignUp = (props) => {
 
     const [form, setValues] = useState({
         EMAIL: '',
-      });
-    
-      const handleInput = (event) => {
+    });
+
+    const handleInput = (event) => {
         setValues({
-          ...form,
-          [event.target.name]: event.target.value,
+            ...form,
+            [event.target.name]: event.target.value,
         });
         console.log(form)
-      };
-    
-      const handlSubmit = (event) => {
-        event.preventDefault();
-        Firebase.
-        //props.history.push('/');//
-      };
-    
-    return(
+    };
+
+    const handlSubmit = (event) => {
+        event.preventDefault(); //quitar el defecto del comportamiento de un formulario
+        Firebase.prototype.createUserWithEmailAndPassword(form, '/prueba')
+            .then(() => {
+            console.log('formulario registrado con exito.')
+            props.history.push('/login');// rediriges a la pagina que quiero que se muestre si el formulario fue efectivo.
+            })
+            .catch(() => {
+            console.log('algo salio mal')
+        })
+        // Ejecutar el registro de auth para usuerio
+    };
+
+    const LoginGoogle = () => {
+        Firebase.prototype.loginGoogle().then(() => {
+            console.log('se cumplio login con google')
+            props.history.push('/home');//para donde me envia si se cumple el logeo
+        })
+    }
+    return (
 
         <section className="section-sign">
-            <Logo/>
+            <Logo />
             <form onSubmit={handlSubmit}>
                 <div className="div-inputssing">
-                    <div className="div-spansigsig">   
+                    <div className="div-spansigsig">
                         <span className="labelsing">E-mail:</span>
                     </div>
                     <input type="text" name="EMAIL" id="" onChange={handleInput} />
                 </div>
                 <div className="div-inputssing">
-                    <div className="div-spansig">   
+                    <div className="div-spansig">
                         <span className="labelsing">Password:</span>
                     </div>
                     <input type="text" name="PASSWORD" id="" onChange={handleInput} />
                 </div>
                 <div className="div-inputssing">
-                    <div className="div-spansig">   
+                    <div className="div-spansig">
                         <span className="labelsing">Repeat Password:</span>
                     </div>
-                    <input type="text" name="" id="" onChange={handleInput}/>
+                    <input type="text" name="" id="" onChange={handleInput} />
                 </div>
                 <div className="div-inputssing">
-                    <div className="div-spansig">   
+                    <div className="div-spansig">
                         <span className="labelsing">Mobile Number:</span>
                     </div>
                     <input type="text" name="" id="" onChange={handleInput} />
                 </div>
                 <div className="div-inputssing">
-                    <div className="div-spansig">   
+                    <div className="div-spansig">
                         <span className="labelsing">City:</span>
                     </div>
                     <select className="selectsign" name="select" onChange={handleInput}>
-                        <option value="value1">Los Angeles</option> 
+                        <option value="value1">Los Angeles</option>
                         <option value="value2" >Miami</option>
                         <option value="value3">Florida</option>
                     </select>
                 </div>
                 <div className="div-button">
-                        <button type="submit">Next </button>
+                    <button type="submit">Next </button>
                 </div>
-        </form>
-    </section>
+            </form>
+            <button type='button' onClick={() => LoginGoogle()}>Google</button>
+        </section>
     )
 }
 
